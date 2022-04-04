8e40bebf986c384a17b2e135d9e950bcfe12a128
Buse Hizarci <78765003+BuseHizarci@users.noreply.github.com>
2022-04-04 06:35:00 +0100

    component str.

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import {View , Text} from 'react-native';
+import {View , Text , SafeAreaView } from 'react-native';
 //React native bir proje olusturdugumda React Native mimarisi olan bir Node.js insaa ederiz //
 
 //View Web'deki div e karsilik gelir
 // Text <p> tagine karsilik gelir
 
-funtion App () {
+function App () {
   return(
+    <SafeAreaView>
     <View>
       <Text>
         Hello React Native World! 01.04.2022 London- UK
       </Text>
+      <Text> Text 2 - Text2'te bu View in childi oluyor.</Text>
     </View>
+    <Text> View in icinde olmayan bir text
+    </Text>
+    </SafeAreaView>
+    
   )
 }
 export default App;
-//Default = Fonksiyon olusturup App diyerek gonderiyorum. Bu index.js dosyamda kullaniliyor. 
+//Text2'te bu View in childi oluyor.
+//Sentetic sugar :
+// Export - Default = Fonksiyon olusturup App diyerek gonderiyorum. Bu index.js dosyamda kullaniliyor. 
