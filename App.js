24fa477089e31af2194a711c03181f8c6ebe47e0
Buse Hizarci <78765003+BuseHizarci@users.noreply.github.com>
2022-04-19 21:33:26 +0100

    Custom Component Done

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View, Text, SafeAreaView, Button , StyleSheet} from 'react-native';
+import { View, Text, SafeAreaView, Button , StyleSheet, TouchableOpacity} from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 //React native bir proje olusturdugumda React Native mimarisi olan bir Node.js insaa ederiz //
 
 //View Web'deki div e karsilik gelir
-// Text <p> tagine karsilik gelir
-
+//Text <p> tagine karsilik gelir
+//Oyle bir View ki icine eklediklerine basilabilir ->  Touchable opacity 
+//Click event
 function App() {
   
   return (
@@ -15,6 +16,9 @@ function App() {
          <Text style={styles.card_title}>Edard Stark</Text>
          <Text style={styles.card_text}>Winter is Coming</Text>
     </View>
+   <TouchableOpacity style={styles.card_button_container}>
+   <Text style={styles.card_button_title}> I LIKED</Text>
+   </TouchableOpacity>
     </View>
     </SafeAreaView>
 
@@ -24,12 +28,12 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     backgroundColor:'grey',
-    borderRadius:4,
+    borderRadius:10,
   },
   card_container:{
     backgroundColor:'white',
     borderWidth:4,
-    borderRadius:5,
+    borderRadius:10,
   },
   card_body: {
     padding:50,
@@ -43,10 +47,17 @@ const styles = StyleSheet.create({
   card_text:{
     margin:5,
     alignItems:'center',
-  }
-  
-
+  },
+  card_button_container:{
+  backgroundColor:'blue',
+  padding:10,
+  alignItems:'center',
+  borderBottomRadius:10,
+  },
+ card_button_title:{
+   fontWeight:'bold',
+   fontSize:19,
+  color:'white'
+ }
 })
 export default App;
-//rowdan columna almak icin layout propslarin icine bak.
-//Dikey yapiya hizalayacagim
